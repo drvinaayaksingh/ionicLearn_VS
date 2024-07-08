9b54b1e68957b7b541bea6d1a73fd851b031a96a
Dr. Vinaayak Singh <tiffinshare.vs@gmail.com>
2024-07-08 21:19:09 +0530

    Initial Project

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+})
+export class AppComponent {
+  constructor() {}
+
+  get showSidebarMenu(): boolean {
+    let str = window.location.href;
+    let param = str.split('/');
+    return param.includes('tab2');
+  }
+  get showSidebarMenuForProject(): boolean {
+    let str = window.location.href;
+    let param = str.split('/');
+    return param.includes('tab3');
+  }
+}
